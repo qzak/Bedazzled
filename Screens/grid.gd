cc269dedb561f5237975601b8ee4d8bc5fc2110b
Qzak <kozik1995@gmail.com>
2026-04-17 23:56:50 +0100

    add logic so initial screen has no matches

--- Screens/grid.gd
+++ Screens/grid.gd
@@ -14,17 +14,18 @@ var gem_list = [
 	preload("res://Objects/aquamarine.tscn")
 ]
 
-var all_pieces = []
+var gem_array = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var gem_array = make_grid_array()
+	gem_array = make_grid_array()
 	print(gem_array)
-	print(randi() % gem_list.size())
-	for i in gridWidth:
-		for j in gridHeight:
-			spawn_gem(i, j)
-	
+	for column in gridHeight:
+		for row in gridWidth:
+			print("Spawning gem at column: " + str(column) + " row: " + str(row))
+			print("gem array location: " + str(gem_array[column][row]))
+			spawn_gem(column, row)
+
 func grid_to_pixel(column, row):
 	var new_x = start.x + offset * column
 	var new_y = start.y + -offset * row
@@ -35,14 +36,44 @@ func make_grid_array():
 	for i in gridWidth:
 		array.append([])
 		for j in gridHeight:
-			array[i].append(null)
+			array[i].append("temp")
 	return array
 
 func spawn_gem(column, row):
 	var random_gem = randi() % gem_list.size()
 	var gem_instance = gem_list[random_gem].instantiate()
+	var random_gem_type = gem_instance.gemType
+	if check_match(column, row, random_gem_type):
+		if random_gem == gem_list.size() - 1:
+			random_gem = 0
+		else:
+			random_gem += 1
+		gem_instance = gem_list[random_gem].instantiate()
+		random_gem_type = gem_instance.gemType
+		if check_match(column, row, random_gem_type):
+			if random_gem == gem_list.size() - 1:
+				random_gem = 0
+			else:
+				random_gem += 1
+			gem_instance = gem_list[random_gem].instantiate()
+			random_gem_type = gem_instance.gemType
 	add_child(gem_instance)
 	gem_instance.position = grid_to_pixel(column, row)
+	print ("Spawned gem at column: " + str(column) + " row: " + str(row) + " of type: " + str(random_gem))
+	gem_array[column][row] = gem_instance
+
+func check_match(column, row, gemType):
+	if column > 1:
+		if gem_array[column - 1][row] != null and gem_array[column - 2][row] != null:
+			if gemType == gem_array[column - 1][row].gemType and gemType == gem_array[column - 2][row].gemType:
+				return true
+	if row > 1:
+		if gem_array[column][row - 1] != null and gem_array[column][row - 2] != null:
+			if gemType == gem_array[column][row - 1].gemType and gemType == gem_array[column][row - 2].gemType:
+				return true
+	return false
+
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
