36af8900f54904425b99076bcc9b08aa8ff80025
Qzak <kozik1995@gmail.com>
2026-04-19 22:36:42 +0100

    add gem matching algo and adjust the action to be tracked every frame to stop double-triggering of mouse click events

--- Objects/gem.gd
+++ Objects/gem.gd
@@ -3,7 +3,7 @@ extends Node2D
 @export var gemType = ""
 
 var defaultScale = 0.2
-
+var matched = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -22,11 +22,7 @@ func _process(delta: float) -> void:
 	else:
 		$AnimatedSprite2D.scale = Vector2(defaultScale, defaultScale)
 
-
-#func _on_area_2d_mouse_entered():
-#	var tween = create_tween()
-#	tween.tween_property($AnimatedSprite2D, "scale", Vector2(0.3, 0.3), 0.3)
-#
-#func _on_area_2d_mouse_exited() -> void:
-#	var tween = create_tween()
-#	tween.tween_property($AnimatedSprite2D, "scale", Vector2(0.2, 0.2), 0.3)
+func match():
+	matched = true
+	#make gem semi-transparent to indicate it has been matched
+	$AnimatedSprite2D.modulate = Color(1, 1, 1, 0.5)
--- Screens/grid.gd
+++ Screens/grid.gd
@@ -3,8 +3,9 @@ extends Node2D
 #Grid vars
 var gridWidth = 10
 var gridHeight = 10
-var start = Vector2(760,1000)
+var grid_start = Vector2(760,1000)
 var offset = 100
+var swap_in_progress = false
 
 #Gem vars
 var gem_list = [
@@ -20,7 +21,10 @@ var gem_array = []
 #Action vars
 var action_start = Vector2()
 var action_end = Vector2()
-var action_active = false
+var action_start_grid_pos = Vector2()
+var action_end_grid_pos = Vector2()
+var direction = Vector2()
+var action_legal = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -33,13 +37,13 @@ func _ready():
 			spawn_gem(column, row)
 
 func grid_to_pixel(column, row):
-	var new_x = start.x + offset * column
-	var new_y = start.y + -offset * row
+	var new_x = grid_start.x + offset * column
+	var new_y = grid_start.y + -offset * row
 	return Vector2(new_x, new_y)
 
-func pixel_to_grid(position):
-	var column = int((position.x - start.x + offset / 2) / offset)
-	var row = int((start.y - position.y + offset / 2) / offset)
+func pixel_to_grid(pos):
+	var column = int((pos.x - grid_start.x + offset / 2) / offset)
+	var row = int((grid_start.y - pos.y + offset / 2) / offset)
 	return Vector2(column, row)
 
 func make_grid_array():
@@ -89,32 +93,97 @@ func is_in_grid(column, row):
 		return false
 	return true
 
-func _input(event):
-	if Input.is_action_just_pressed("mouse_click"):
-		action_start = get_global_mouse_position()
-		print("Action start: " + str(action_start))
-		var grid_pos = pixel_to_grid(action_start)
-		if is_in_grid(grid_pos.x, grid_pos.y):
-			action_active = true
-	if Input.is_action_just_released("mouse_click"):
-		action_end = get_global_mouse_position()
-		print("Action end: " + str(action_end))
-		var grid_pos = pixel_to_grid(action_end)
-		if is_in_grid(grid_pos.x, grid_pos.y) && action_active:
-			if action_start.distance_to(action_end) > 50:
-				swap_gems(pixel_to_grid(action_start).x, pixel_to_grid(action_start).y, pixel_to_grid(action_end).x, pixel_to_grid(action_end).y)
-			print("Action from " + str(pixel_to_grid(action_start)) + " to " + str(pixel_to_grid(action_end)))
+func check_matches():
+	var found_at_least_one_match = false
+	for column in gridWidth:
+		for row in gridHeight:
+			if gem_array[column][row] != null:
+				var gemType = gem_array[column][row].gemType
+				if column > 1:
+					if gem_array[column - 1][row] != null and gem_array[column - 2][row] != null:
+						if gemType == gem_array[column - 1][row].gemType and gemType == gem_array[column - 2][row].gemType:
+							print("Match found at column: " + str(column) + " row: " + str(row))
+							gem_array[column][row].match()
+							gem_array[column - 1][row].match()
+							gem_array[column - 2][row].match()
+							found_at_least_one_match = true
+				if row > 1:
+					if gem_array[column][row - 1] != null and gem_array[column][row - 2] != null:
+						if gemType == gem_array[column][row - 1].gemType and gemType == gem_array[column][row - 2].gemType:
+							print("Match found at column: " + str(column) + " row: " + str(row))
+							gem_array[column][row].match()
+							gem_array[column][row - 1].match()
+							gem_array[column][row - 2].match()
+							found_at_least_one_match = true
+	if found_at_least_one_match:
+		return true
+	else:
+		return false
 
-func swap_gems(column1, row1, column2, row2):
+func movement_direction(start, end):
+	var mov_direction = end - start
+	if abs(mov_direction.x) > abs(mov_direction.y):
+		mov_direction.y = 0
+		if mov_direction.x > 0:
+			mov_direction.x = 1
+		else:
+			mov_direction.x = -1
+	else:
+		mov_direction.x = 0
+		if mov_direction.y > 0:
+			mov_direction.y = -1
+		else:
+			mov_direction.y = 1
+	return mov_direction
+
+func swap_gems(column1, row1, swap_direction):
 	var first_gem = gem_array[column1][row1]
-	var second_gem = gem_array[column2][row2]
-	var temp = gem_array[column1][row1]
-	gem_array[column1][row1] = gem_array[column2][row2]
-	gem_array[column2][row2] = temp
-	first_gem.position = grid_to_pixel(column2, row2)
-	second_gem.position = grid_to_pixel(column1, row1)
+	var second_gem = gem_array[column1 + swap_direction.x][row1 + swap_direction.y]
+	print("Swap gems triggered, first gem: " + str(first_gem) + " second gem: " + str(second_gem))
+	gem_array[column1][row1] = second_gem
+	gem_array[column1 + swap_direction.x][row1 + swap_direction.y] = first_gem
+	print("Gems swapped in array")
+	var first_gem_tween = get_tree().create_tween()
+	first_gem_tween.tween_property(first_gem, "position", grid_to_pixel(column1 + swap_direction.x, row1 + swap_direction.y), 0.5)
+	var second_gem_tween = get_tree().create_tween()
+	second_gem_tween.tween_property(second_gem, "position", grid_to_pixel(column1, row1), 0.5)
+	print("Gem animations triggered")
+	if check_matches():
+		print("Match found, keeping swap")
+		await first_gem_tween.finished
+		#score_matches()
+		#temporary resolution
+		swap_in_progress = false
+	else:
+		print("No match found, swapping back")
+		first_gem_tween.tween_property(first_gem, "position", grid_to_pixel(column1, row1), 0.5)
+		second_gem_tween.tween_property(second_gem, "position", grid_to_pixel(column1 + swap_direction.x, row1 + swap_direction.y), 0.5)
+		gem_array[column1][row1] = first_gem
+		gem_array[column1 + swap_direction.x][row1 + swap_direction.y] = second_gem
+		#wait until the tweens are done before allowing another swap
+		await first_gem_tween.finished
+		swap_in_progress = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if swap_in_progress == false:
+		if Input.is_action_just_pressed("mouse_click"):
+			print("mouse clicked")
+			action_start = get_global_mouse_position()
+			print("Action start: " + str(action_start))
+			action_start_grid_pos = pixel_to_grid(action_start)
+			if is_in_grid(action_start_grid_pos.x, action_start_grid_pos.y):
+				action_legal = true
+		if Input.is_action_just_released("mouse_click"):
+			print("mouse released")
+			action_end = get_global_mouse_position()
+			action_end_grid_pos = pixel_to_grid(action_end)
+			print("Action end: " + str(action_end) + " End grid pos: " + str(action_end_grid_pos))
+			if action_legal:
+				print ("Action start: " + str(action_start_grid_pos) + " Action end: " + str(action_end_grid_pos))
+				direction = movement_direction(action_start, action_end)
+				if !((direction.x + action_start_grid_pos.x) >= gridWidth or (direction.y + action_start_grid_pos.y) >= gridHeight or (direction.x + action_start_grid_pos.x) < 0 or (direction.y + action_start_grid_pos.y) < 0 or action_start == action_end):
+					swap_gems(action_start_grid_pos.x, action_start_grid_pos.y, direction)
+					swap_in_progress = true
+			action_legal = false
